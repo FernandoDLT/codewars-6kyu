d39a2bc7b7b2bdc68afdb706fa83dfb38cea2eee
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2022-08-06 15:15:01 -0500

    PREP multiples of 3 or 5 More practice

---
+++ main.js
@@ -0,0 +1,27 @@
+// function solution(number){
+//   if(number <= 0) return 0;
+//   for(let i = )
+// }
+/* 
+PREP
+Parameters
+One perameter 
+Number can be negative
+
+RETURNS:
+Return the sum of ll the numbers from 0 to the given numbers are divisible by 3 and or 5
+
+EXAMPLES:
+Solution (10) // return 23 3+5+6+9
+Solution (-10) // return 0
+Solution (20) // return 78 3+5+6+9+10+12+15+18
+*/
+function solution(number){
+    let counter = 0
+    for(let i = 3; i < number; i++) {
+        if(i % 3 === 0 || i % 5 === 0) {
+            counter += i
+        }
+    }
+    return counter
+}
